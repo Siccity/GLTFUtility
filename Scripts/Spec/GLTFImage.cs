859816db21022b8143112beb2db6e4e1bae50255
Thor Brigsted <thorbrigsted@gmail.com>
2019-10-06 02:33:02 +0200

    Streamlined GLTFImage import

--- Scripts/Spec/GLTFImage.cs
+++ Scripts/Spec/GLTFImage.cs
@@ -16,77 +16,69 @@ namespace Siccity.GLTFUtility {
 		/// The image format must be jpg or png.
 		/// </summary>
 		public string uri;
+		/// <summary> Either "image/jpeg" or "image/png" </summary>
 		public string mimeType;
 		public int? bufferView;
 		public string name;
 
 		public class ImportResult {
 			public Texture2D texture;
-			/// <summary> True if image was loaded from a Texture2D asset. False if it was loaded from binary or from another source </summary>
-			public bool isNormalMap;
-			public bool isMetallicRoughnessFixed;
-			public byte[] bytes;
-			public string mimeType;
-
-			// glTF stores Metallic in blue channel and roughness in green channel. Unity stores Metallic in red and roughness in alpha. This method returns a unity-fixed texture
-			/* public Texture2D GetFixedMetallicRoughness() {
-				if (!isMetallicRoughnessFixed) {
-					Color32[] pixels = texture.GetPixels32();
-					for (int i = 0; i < pixels.Length; i++) {
-						Color32 c = pixels[i];
-						c.r = pixels[i].b;
-						c.a = pixels[i].g;
-						pixels[i] = c;
-					}
-					texture.SetPixels32(pixels);
-					texture.Apply();
-					isMetallicRoughnessFixed = true;
-				}
-				return texture;
-			} */
-		}
-
-		public ImportResult GetImage(string directoryRoot, GLTFBufferView.ImportResult[] bufferViews) {
-			ImportResult result = new ImportResult();
-
-			if (!string.IsNullOrEmpty(uri) && File.Exists(directoryRoot + uri)) {
-				byte[] fileData = File.ReadAllBytes(directoryRoot + uri);
-				result.bytes = fileData;
-				result.mimeType = mimeType;
-				return result;
-			} else if (bufferView.HasValue && !string.IsNullOrEmpty(mimeType)) {
-				byte[] bytes = bufferViews[bufferView.Value].bytes;
-				result.bytes = bytes;
-				result.mimeType = mimeType;
-				return result;
-			} else {
-				Debug.Log("Couldn't find texture at " + directoryRoot + uri);
-				return null;
-			}
 		}
 
 		public class ImportTask : Importer.ImportTask<ImportResult[]> {
+			private ImageData[] imageData;
+
+			private class ImageData {
+				public byte[] bytes;
+				public string path;
+
+				public ImageData(byte[] bytes, string path = null) {
+					this.bytes = bytes;
+					this.path = path;
+				}
+
+				public Texture2D ToTexture2D() {
+					Texture2D tex = new Texture2D(2, 2);
+					if (!tex.LoadImage(bytes)) {
+						Debug.Log("mimeType not supported");
+						return null;
+					}
+					return tex;
+				}
+			}
+
 			public ImportTask(List<GLTFImage> images, string directoryRoot, GLTFBufferView.ImportTask bufferViewTask) : base(bufferViewTask) {
 				task = new Task(() => {
 					// No images
 					if (images == null) return;
 
-					Result = new ImportResult[images.Count];
-					for (int i = 0; i < Result.Length; i++) {
-						Result[i] = images[i].GetImage(directoryRoot, bufferViewTask.Result);
+					imageData = new ImageData[images.Count];
+					for (int i = 0; i < imageData.Length; i++) {
+						string fullUri = directoryRoot + images[i].uri;
+						if (!string.IsNullOrEmpty(images[i].uri) && File.Exists(fullUri)) {
+							byte[] bytes = File.ReadAllBytes(fullUri);
+							imageData[i] = new ImageData(bytes, images[i].uri);
+						} else if (images[i].bufferView.HasValue && !string.IsNullOrEmpty(images[i].mimeType)) {
+							byte[] bytes = bufferViewTask.Result[images[i].bufferView.Value].bytes;
+							imageData[i] = new ImageData(bytes);
+						} else {
+							Debug.Log("Couldn't find texture at " + fullUri);
+						}
 					}
 				});
 			}
 
 			protected override void OnMainThreadFinalize() {
 				// No images
-				if (Result == null) return;
+				if (imageData == null) return;
 
-				foreach (ImportResult result in Result) {
-					result.texture = new Texture2D(2, 2);
-					if (!result.texture.LoadImage(result.bytes)) {
-						Debug.Log("mimeType not supported: " + result.mimeType);
+				Result = new ImportResult[imageData.Length];
+				for (int i = 0; i < imageData.Length; i++) {
+					if (imageData[i] == null) {
+						Debug.LogWarning("imageData[" + i + "] is null");
+						continue;
 					}
+					Result[i] = new ImportResult() { texture = imageData[i].ToTexture2D() };
 				}
 			}
 		}
